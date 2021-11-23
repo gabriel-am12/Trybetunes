b456c4f94896cbdcba693461f108e0960a59ee72
Gabriel A <gabrielaug4@gmail.com>
2021-11-23 16:47:19 -0300

    feat: req.08

--- src/pages/MusicCard.jsx
+++ src/pages/MusicCard.jsx
@@ -3,29 +3,35 @@ import PropTypes from 'prop-types';
 
 class MusicCard extends React.Component {
   render() {
-    const { musics } = this.props;
+    const { musicList, handleChange, isChecked } = this.props;
+    const { trackName, previewUrl, trackId } = musicList;
     return (
-      <ul>
-        {musics.map(({ trackName, previewUrl }, index) => (
-          (index === 0)
-            ? null : (
-              <li key={ index }>
-                <span>{ trackName }</span>
-                <audio src={ previewUrl } data-testid="audio-component" controls>
-                  <track kind="captions" />
-                  O seu navegador não suporta o elemento
-                  <code>audio</code>
-                </audio>
-              </li>
-            )
-        ))}
-      </ul>
+      <section>
+        <h3>{trackName}</h3>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+        </audio>
+        <label htmlFor="fav-checkbox">
+          <input
+            type="checkbox"
+            data-testid={ `checkbox-music-${trackId}` }
+            id="fav-checkbox"
+            onChange={ handleChange }
+            checked={ isChecked }
+          />
+          Favorita
+        </label>
+      </section>
     );
   }
 }
 
 MusicCard.propTypes = {
-  musics: PropTypes.object,
-}.isRequire;
+  musicList: PropTypes.objectOf(PropTypes.any).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  isChecked: PropTypes.bool.isRequired,
+};
 
 export default MusicCard;
