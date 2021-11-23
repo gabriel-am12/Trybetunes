75ba25b49c9f52d6f45ba666a1b245852016d171
Gabriel A <gabrielaug4@gmail.com>
2021-11-23 16:47:19 -0300

    req.08-11

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,54 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from './MusicCard';
+import { addSong, removeSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
-class Album extends React.Component {
-  constructor() {
-    super();
+class Album extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
     this.state = {
-      artistName: '',
-      collectionName: '',
-      musics: [],
-      image: '',
+      musicList: [],
+      artistInfo: {},
+      favMusics: [],
+      loading: false,
     };
   }
 
   componentDidMount() {
-    const { match: { params: { id } } } = this.props;
-    getMusics(id)
-      .then((response) => {
-        const { artistName, collectionName, artworkUrl100: image } = response[0];
-        this.setState({
-          artistName,
-          collectionName,
-          musics: response,
-          image,
-        });
+    this.fetchMusic();
+    this.renderFavoritesSongs();
+  }
+
+  async handleChange(music) {
+    const { favMusics } = this.state;
+    let favArray = [...favMusics];
+    const haveId = favArray.some(({ trackId }) => trackId === music.trackId);
+    this.setState({ loading: true });
+
+    if (haveId) {
+      await removeSong(music);
+      favArray = favArray.filter(({ trackId }) => trackId !== music.trackId);
+    } else {
+      await addSong(music);
+      favArray = [...favMusics, music];
+    }
+    this.setState({ loading: false, favMusics: favArray });
+  }
+
+  fetchMusic = async () => {
+    this.setState({
+      loading: true,
+    }, async () => {
+      const { match: { params: { id } } } = this.props;
+      const data = await getMusics(id);
+      this.setState({
+        loading: false,
+        artistInfo: data[0],
+        musicList: data,
+
       });
+    });
+  }
+
+  renderFavoritesSongs = () => {
+    this.setState({
+      loading: true }, async () => {
+      const fav = await getFavoriteSongs();
+      this.setState({ favMusics: fav, loading: false });
+    });
   }
 
   render() {
-    const { artistName, collectionName, musics, image } = this.state;
+    const { musicList,
+      loading,
+      artistInfo: { artworkUrl100, collectionName, artistName },
+      favMusics } = this.state;
     return (
-      <div data-testid="page-album">
+      <>
         <Header />
-        <div>
-          <div>
-            <img src={ image } alt={ collectionName } />
-            <h3 data-testid="album-name">{ collectionName }</h3>
-            <p data-testid="artist-name">{ artistName }</p>
-          </div>
-          <MusicCard musics={ musics } />
+        <div data-testid="page-album">
+          {loading ? <p>Carregando...</p> : (
+            <>
+              <section>
+                <img src={ artworkUrl100 } alt={ collectionName } />
+                <h2 data-testid="album-name">{collectionName}</h2>
+                <p data-testid="artist-name">{artistName}</p>
+              </section>
+              {
+                musicList.filter(({ kind }) => kind === 'song')
+                  .map((music) => (<MusicCard
+                    key={ music.trackId }
+                    musicList={ music }
+                    isChecked={ favMusics.some(
+                      ({ trackId }) => trackId === music.trackId,
+                    ) }
+                    handleChange={ () => this.handleChange(music) }
+                  />))
+              }
+            </>
+          )}
         </div>
-      </div>
+      </>
     );
   }
 }
 
 Album.propTypes = {
-  id: PropTypes.number,
-}.isRequire;
+  match: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 export default Album;
